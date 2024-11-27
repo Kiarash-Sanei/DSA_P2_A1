c2169ee768edc8bbf9e0498fe70a9f8363222ea7
Kiarash Sanei <kiarashsanei5@gmail.com>
2024-11-27 22:17:13 +0330

    second attempt

---
+++ 2.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int sortMinHeap(int *x, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (2 * i + 1 < size && x[2 * i + 1] < x[i])
+        {
+            swap(x + 2 * i + 1, x + i);
+        }
+        if (2 * i + 2 < size && x[2 * i + 2] < x[i])
+        {
+            swap(x + 2 * i + 2, x + i);
+        }
+    }
+}
+
+int sortMaxHeap(int *x, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (2 * i + 1 < size && x[2 * i + 1] > x[i])
+        {
+            swap(x + 2 * i + 1, x + i);
+        }
+        if (2 * i + 2 < size && x[2 * i + 2] > x[i])
+        {
+            swap(x + 2 * i + 2, x + i);
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    scanf("%d\n", &n);
+    int minHeap[100000];
+    minHeap[0] = 0;
+    int maxHeap[100000];
+    maxHeap[0] = 0;
+    int minIndex = 0;
+    int maxIndex = 0;
+    long long c = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int command;
+        scanf("%d", &command);
+
+        if (command == 1)
+        {
+            int a, b;
+            scanf(" %d %d\n", &a, &b);
+
+            if (a <= maxHeap[0])
+            {
+                maxHeap[maxIndex] = a;
+                maxIndex++;
+                sortMaxHeap(maxHeap, maxIndex);
+            }
+            else
+            {
+                minHeap[minIndex] = a;
+                minIndex++;
+                sortMinHeap(minHeap, minIndex);
+            }
+
+            c += b;
+        }
+        else
+        {
+            int median = maxIndex >= minIndex ? maxHeap[0] : minHeap[0];
+            long long sum = 0;
+
+            for (int j = 0; j < minIndex; j++)
+            {
+                sum += llabs(median - minHeap[j]);
+            }
+            for (int j = 0; j < maxIndex; j++)
+            {
+                sum += llabs(median - maxHeap[j]);
+            }
+
+            printf("%d %lld\n", median, sum + c);
+        }
+    }
+
+    return 0;
+}
---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Please enter the version you want to test:"
+read n
+gcc $n.c -o $n.out -lm
+for file in *.txt
+do
+    echo "testing $n, testcase $file:"
+    start=$(date +%s%N)
+    ./$n.out < $file
+    end=$(date +%s%N)
+    time_ns=$((end - start))
+    time_ms=$(echo "scale=3; $time_ns / 1000000" | bc)
+    echo "execution time: $time_ns ns"
+    echo "execution time: $time_ms ms"
+done
