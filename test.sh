c4adcf44447ab5420d72dbc82b282d991bd9d101
Kiarash Sanei <kiarashsanei5@gmail.com>
2024-11-27 23:04:01 +0330

    improved test

--- test.sh
+++ test.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-echo "Please enter the version you want to test:"
-read n
-gcc $n.c -o $n.out -lm
-for file in *.txt
+n=$(find . -maxdepth 1 -name "*.c" | wc -l)
+for ((i=1; i<=$n; i++))
 do
-    echo "testing $n, testcase $file:"
-    start=$(date +%s%N)
-    ./$n.out < $file
-    end=$(date +%s%N)
-    time_ns=$((end - start))
-    time_ms=$(echo "scale=3; $time_ns / 1000000" | bc)
-    echo "execution time: $time_ns ns"
-    echo "execution time: $time_ms ms"
+    echo "***************************"
     echo "---------------------------"
+    gcc $i.c -o $i.out -lm
+    cd ./tests
+    m=$((1))
+    for file in *.txt
+    do
+        echo "testing $i, testcase $m:"
+        start=$(date +%s%N)
+        ../$i.out < $file
+        end=$(date +%s%N)
+        time_ns=$((end - start))
+        time_ms=$(echo "scale=3; $time_ns / 1000000" | bc)
+        echo "execution time: $time_ns ns"
+        echo "execution time: $time_ms ms"
+        ((m++))
+        echo "---------------------------"
+    done
+    cd ..
+    rm $i.out
+    echo "***************************"
 done
